4c99d08a08d06013278b283e7d27e1fbbb66a5c4
artttnik <artttnik@gmail.com>
2026-03-02 13:07:20 +0300

    lab7: init

---
+++ sem4/lab7/COMPARATOR/Comparator.v
@@ -0,0 +1,27 @@
+module Comparator (
+    input [3:0] A,
+    input [3:0] B,
+    output reg A_greater_B,
+    output reg A_equal_B,
+    output reg A_less_B
+);
+
+always @(*) begin
+    if (A > B) begin
+        A_greater_B = 1'b1;
+        A_equal_B   = 1'b0;
+        A_less_B    = 1'b0;
+    end
+    else if (A == B) begin
+        A_greater_B = 1'b0;
+        A_equal_B   = 1'b1;
+        A_less_B    = 1'b0;
+    end
+    else begin
+        A_greater_B = 1'b0;
+        A_equal_B   = 1'b0;
+        A_less_B    = 1'b1;
+    end
+end
+
+endmodule
